a8e4ab56c532dac1b180db96a92d974af3efb630
Khaled Maher <khaled.mailhub@gmail.com>
2023-10-02 18:15:45 +0300

    init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,39 @@
 import { useEffect, useCallback, useState } from 'react';
 
+/**
+ * send subscription to backend to store
+ */
+async function sendSubscriptionToBackEnd(subscription: PushSubscription) {
+  const response = await fetch('https://gentle-island-01019-ea84504a6e5e.herokuapp.com/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(subscription),
+  });
+  if (!response.ok) {
+    throw new Error('Bad status code from server.');
+  }
+  console.log(response)
+  // const responseData = await response.json();
+  // if (!(responseData.data && responseData.data.success)) {
+  //   throw new Error('Bad response from server.');
+  // }
+}
 export default function Home() {
   const [subscription, setSubscription] = useState({});
 
-  /**
-   *  Ask for notification permission
-   */
-  const askPermission = useCallback((): Promise<NotificationPermission> => Notification.requestPermission(), [])
 
-
-  /**
-   * send subscription to backend to store
-   */
-  async function sendSubscriptionToBackEnd(subscription: PushSubscription) {
-    const response = await fetch('https://gentle-island-01019-ea84504a6e5e.herokuapp.com/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(subscription),
-    });
-    if (!response.ok) {
-      throw new Error('Bad status code from server.');
-    }
-    console.log(response)
-    // const responseData = await response.json();
-    // if (!(responseData.data && responseData.data.success)) {
-    //   throw new Error('Bad response from server.');
-    // }
-  }
+  /** 
+   * unsubscribe from current subscription
+   */ 
+  // const unsubscribe = useCallback(async () => {
+  //   const subscirption: PushSubscription | null = await registration.pushManager.getSubscription()
+  //   subscription && await subscirption?.unsubscribe()
+  // }, [])
 
   const registerServiceWorker = useCallback(() => {
+      if (!navigator.serviceWorker) return;
       return navigator.serviceWorker
         .register('/service-worker.js')
         .then((registration) => {
@@ -39,9 +42,7 @@ export default function Home() {
             userVisibleOnly: true,
             applicationServerKey: 'BKuoQRQtmQxFY0QVySzagevEMO0gMw8iVIpEtj4bgCX1EQb_xcsKrWb4p-agefCYgi5aARZMZEuF5QsZrQAw63E'
           };
-          // unsubscribe from current subscription
-          // const subscirption: PushSubscription | null = await registration.pushManager.getSubscription()
-          // subscription && await subscirption?.unsubscribe()
+
           registration.pushManager.subscribe(subscribeOptions).then((pushSubscription) => {
             console.log('Received PushSubscription: ', JSON.stringify(pushSubscription));
             setSubscription(pushSubscription)
@@ -52,45 +53,42 @@ export default function Home() {
         .catch((err) => {
           console.error('Unable to register service worker.', err);
         })
+
+        
   }, [])
 
 
-  function urlBase64ToUint8Array(string: string) {
-    const base64String = string || Buffer.from(string).toString('base64')
-    const padding = '='.repeat((4 - base64String.length % 4) % 4);
-    const base64 = (base64String + padding)
-      .replace(/\-/g, '+')
-      .replace(/_/g, '/')
-    ;
-    const rawData: string = window.atob(base64);
-    return Uint8Array.from(rawData.split('').map((char) => char.charCodeAt(0)));
-  }
-
-  /**
-   * ask for permission
-   */
-  useEffect(() => {
-    if (!("Notification" in window)) {
-      console.log('what is permission', Notification.permission)
-      alert("This browser does not support desktop notification");
-    } else if (Notification.permission === "granted") {
-      new Notification("Hi there!");
-    } else if (Notification.permission !== "denied") {
-      askPermission().then((permission: NotificationPermission) => {
-        console.log('permission', permission)
+  const askNotificationPermission = useCallback(() => {
+    if (!window.Notification) {
+      return alert("This browser does not support desktop notification");
+    } else if (window.Notification.permission === "granted") {
+      new window.Notification("Hi theee the notification permission has been granted for pingfy!");
+    } else if (window.Notification.permission == "denied") {
+      window.Notification.requestPermission().then((permission: NotificationPermission) => {
         if (permission === "granted") {
           new Notification("Hi there!");
         }
       });
     }
+  }, [])
 
+  const askPermissionAndSubscribe = useCallback(async () => { }, [])
+
+  /**
+   * register a service worker
+   */
+  useEffect((): void => {
+    askNotificationPermission()
     registerServiceWorker();
   }, [])
 
   return (
     <> 
-      <p>permission: </p>
+      <p>version1</p>
+      <button id="notificationButton">Enable Notifications</button>
+      <p>permission: </p>{window.Notification.permission}
       <p>Subscription: </p> {JSON.stringify(subscription)}
+      <button onClick={askPermissionAndSubscribe}>Subscribe</button>
     </>
   );
 }
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,15 +1,19 @@
 console.log('service worker works!!')
 
+/**
+ * Trigger the notification when it arrives from the push service
+ */
 self.addEventListener('push', function(event) {
   if (event.data) {
-  console.log('This push event has data: ', event.data.text());
-  // trigger notification in this service worker
-  self.registration.showNotification("Vibration Sample", {
-    body: event.data.text(),
-    vibrate: [200, 100, 200, 100, 200, 100, 200],
-    tag: "vibration-sample",
-  });
+    console.log('This push event has data: ', event.data.text());
+    // trigger notification in this service worker
+    console.log(self.registration.getNotifications());
+    self.registration.showNotification("Vibration Sample", {
+      body: event.data.text(),
+      vibrate: [200, 100, 200, 100, 200, 100, 200],
+      tag: "vibration-sample",
+    });
   } else {
-  console.log('This push event has no data.');
+    console.log('This push event has no data.');
   }
 });
