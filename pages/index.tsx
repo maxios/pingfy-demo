6553ec4cbd9dc685b9377bc27fc8723a8be709a4
Khaled Maher <khaled.mailhub@gmail.com>
2023-10-08 01:41:26 +0300

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -190,19 +190,7 @@ export default function Home() {
    */
   useEffect(() => {
     // Mobile chrome does not support window notification
-    if (browserType === "Safari" && isMobile) {
-      subscribeButton.current?.addEventListener('click', function() {
-        console.log('button clicked')
-        window.Notification.requestPermission().then((permission: NotificationPermission) => {
-          console.log('permision requested', permission)
-          if (permission === "granted") {
-            new Notification("Permission granted for pingfy!");
-          }
-        });
-      })
-    } else {
-      subscribeButton.current?.addEventListener('click', askPermissionAndSubscribe)
-    };
+    subscribeButton.current?.addEventListener('click', askPermissionAndSubscribe)
 
   }, [subscribeButton.current])
 
